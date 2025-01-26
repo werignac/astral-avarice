0cfd545709de3ef7cb2bd88ba3670c1ab352397d
Archie <archi.faug@gmail.com>
2025-01-26 12:11:49 -0800

    DisplayCashAndIncome
    While in game, the players current cash, and income will be displayed in the top left above the build menu.

--- Assets/Scripts/Components/GameController.cs
+++ Assets/Scripts/Components/GameController.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class GameController : MonoBehaviour
 {
     private GameObject levelObject;
     private GameManager gameManager;
+    [SerializeField] private UIDocument buildDocument;
+
+    private Label cashLabel;
+    private Label incomeLabel;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameManager = new GameManager(this);
+        cashLabel = buildDocument.rootVisualElement.Q("Cash") as Label;
+        incomeLabel = buildDocument.rootVisualElement.Q("Income") as Label;
         if (Data.selectedMission != null)
         {
             levelObject = Instantiate<GameObject>(Resources.Load<GameObject>("Levels/" + Data.selectedMission.name));
@@ -21,6 +28,27 @@ public class GameController : MonoBehaviour
     void Update()
     {
         gameManager.Update(Time.deltaTime);
+    }
+
+    public void UpdateCashAndIncome(int newCash, int newIncome)
+    {
+        cashLabel.text = "" + newCash;
+        incomeLabel.text = "(";
+        if(newIncome > 0)
+        {
+            incomeLabel.text += "+";
+            incomeLabel.style.color = new StyleColor(new Color(0, 0.9f, 0));
+        }
+        else if(newIncome == 0)
+        {
+            incomeLabel.style.color = new StyleColor(new Color(1, 1, 1));
+        }
+        else
+        {
+            incomeLabel.style.color = new StyleColor(new Color(0.9f, 0, 0));
+        }
+        incomeLabel.text += newIncome + ")";
+        
     }
 
     public void EndGame()
