31f2371d2877a447b98a6ea2e1c529a87eb80c4c
werignac <werignac@comcast.net>
2025-02-02 03:05:25 -0800

    Planet Destruction VFX Destroy Themselves

--- Assets/Scripts/Components/VFX/ParticleDestructionVFXComponent.cs
+++ Assets/Scripts/Components/VFX/ParticleDestructionVFXComponent.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class ParticleDestructionVFXComponent : MonoBehaviour
 {
+	[SerializeField] new ParticleSystem particleSystem;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-		ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+		if (particleSystem == null)
+			particleSystem = GetComponent<ParticleSystem>();
 		StartCoroutine(DestructionTimerCoroutine(particleSystem.main.duration + particleSystem.main.startLifetime.constantMax));
     }
 
